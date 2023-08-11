4255a592b9d75a410583eda2c70170ff5a1b1170
Felipe <felipebamartins@hotmail.com>
2023-08-11 19:12:42 -0300

    Haikai

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Você pode crer");
+Console.WriteLine("O pior cego");
+Console.WriteLine("É o que quer ver.");
+Console.WriteLine("");
+Console.WriteLine("Millôr Fernandes");
+
+
